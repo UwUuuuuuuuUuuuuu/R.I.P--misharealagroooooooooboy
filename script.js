91247c2071478955a108ffda698861a8e80e0412
MihailSocialGit <mihailsocial08@gmail.com>
2023-06-15 20:23:13 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+
+$(document).ready(function () {
+    $(window).scroll(function () {
+      /* Скроллировать фоновые слои с разной скоростью */
+      let winTop = $(window).scrollTop();
+      $(".parallax").css(
+        "background-position", "120px "+ (500 - winTop *2) +"px,"+
+        "center " + (-90 -(winTop * 0.5)) + "px"
+      );
+    });
+  });
